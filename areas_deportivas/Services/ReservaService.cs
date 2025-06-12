03426c927d0c2505f5b27592b7c23a7de6e19cd7
luifer991 <luifer991@gmail.com>
2025-06-12 15:41:07 -0500

    Add ReservaService class implementing IReservaService with placeholder methods

---
+++ areas_deportivas/Services/ReservaService.cs
@@ -0,0 +1,13 @@
+
+public class ReservaService : IReservaService
+{
+	public Task CancelarReserva()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task Reservar()
+	{
+		throw new NotImplementedException();
+	}
+}
