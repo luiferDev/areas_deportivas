2b831774043f30c904c1b40f6a83e36e482ed169
luifer991 <luifer991@gmail.com>
2025-06-12 16:26:11 -0500

    Refactor Program.cs to add environment variable configuration and database context setup

--- areas_deportivas/Program.cs
+++ areas_deportivas/Program.cs
@@ -1,15 +1,33 @@
+using areas_deportivas.Models;
+using Microsoft.EntityFrameworkCore;
+
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Configuration.AddEnvironmentVariables();
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION") ??
+	throw new InvalidOperationException("La variable de entorno no está definida o está vacía");
+
+builder.Services.AddDbContext<DeportesDbContext>(options =>
+{
+	options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddScoped<IAreaDeportivaService, AreaDeportivaService>();
+builder.Services.AddScoped<IReservaService, ReservaService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
