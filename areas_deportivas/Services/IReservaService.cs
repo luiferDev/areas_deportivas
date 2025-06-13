67e558f6f59cc9b3a8fa178adaa68d2a096c560d
luifer991 <luifer991@gmail.com>
2025-06-13 14:52:34 -0500

    Refactor IReservaService to include async methods for reservation management

--- areas_deportivas/Services/IReservaService.cs
+++ areas_deportivas/Services/IReservaService.cs
@@ -2,9 +2,11 @@
 /// Defines the contract for reservation-related operations within the application.
 /// Implement this interface to provide methods for managing reservations.
 /// </summary>
+using areas_deportivas.Models.DTO; // Add this line or update with the correct namespace for ReservaDTO
+
 namespace areas_deportivas.Services;
 public interface IReservaService
 {
-	Task Reservar(CrearReservaDto crearReserva);
-	Task CancelarReserva();
+	Task<ReservaRespuestaDto> ReservarAsync(CrearReservaDto crearReserva, int Id, Guid userId);
+	Task CancelarReservaAsync();
 }
