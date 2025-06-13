cf7c31f9090c926ca4d723f40cc982e7980d1cfb
luifer991 <luifer991@gmail.com>
2025-06-12 22:10:16 -0500

    Refactor Usuario model to remove 'required' modifier from Persona property and reorganize properties

--- areas_deportivas/Models/Usuario.cs
+++ areas_deportivas/Models/Usuario.cs
@@ -7,7 +7,15 @@ public partial class Usuario
 {
     public Guid Id { get; set; }
 
-    public required virtual Persona Persona { get; set; }
+    public string Nombre { get; set; } = null!;
 
-    public required virtual ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public Role Role { get; set; }
+
+    public Guid? ReservaId { get; set; }
+
+    public virtual ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
 }
