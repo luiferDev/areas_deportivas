11f418318933eed00b392236ee7297e2779ef4f0
luifer991 <luifer991@gmail.com>
2025-06-13 14:49:33 -0500

    Refactor CrearReservaDto to change HoraInicio and HoraFin properties from DateTimeOffset to TimeOnly

--- areas_deportivas/Models/DTO/CrearReservaDto.cs
+++ areas_deportivas/Models/DTO/CrearReservaDto.cs
@@ -4,13 +4,9 @@ public class CrearReservaDto
 {
 	public DateOnly Fecha { get; set; }
 
-	public DateTimeOffset HoraInicio { get; set; }
+	public TimeOnly HoraInicio { get; set; }
 
-	public DateTimeOffset HoraFin { get; set; }
-
-	public Estado EstadoReserva { get; set; }
-
-	public Guid IdUsuario { get; set; }
+	public TimeOnly HoraFin { get; set; }
 
 	public int IdAreaDeportiva { get; set; }
 }
