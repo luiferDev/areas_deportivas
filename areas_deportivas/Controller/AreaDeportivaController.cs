cc1f9ef8a35cb7c1b3a48ff4ab1decf149f8c052
luifer991 <luifer991@gmail.com>
2025-06-13 08:23:44 -0500

    Add AreaDeportivaController with CrearAreaDeportivaAsync method for area creation

---
+++ areas_deportivas/Controller/AreaDeportivaController.cs
@@ -0,0 +1,40 @@
+using areas_deportivas.Models;
+using areas_deportivas.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace areas_deportivas.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AreaDeportivaController : ControllerBase
+{
+	private readonly DeportesDbContext _context;
+
+	public AreaDeportivaController(DeportesDbContext context)
+	{
+		_context = context;
+	}
+	
+	[HttpPost]
+	public async Task<IActionResult> CrearAreaDeportivaAsync([FromBody] CreateAreaDeportivaDto createAreaDeportiva)
+	{
+		try
+		{
+			var areaDeportiva = new AreaDeportiva
+			{
+				Nombre = createAreaDeportiva.Nombre,
+				TipoArea = createAreaDeportiva.TipoArea,
+				Disponibilidad = true
+			};
+
+			_context.AreaDeportivas.Add(areaDeportiva);
+			await _context.SaveChangesAsync();
+
+			return Ok(areaDeportiva);
+		}
+		catch (Exception ex)
+		{
+			return BadRequest(ex.Message);
+		}
+	}
+}
