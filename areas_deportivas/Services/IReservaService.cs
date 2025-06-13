297e2ac7087efde03cf3ce09cd167f309b2c6d80
luifer991 <luifer991@gmail.com>
2025-06-12 22:11:37 -0500

    Update IReservaService interface to include CrearReservaDto parameter in Reservar method

--- areas_deportivas/Services/IReservaService.cs
+++ areas_deportivas/Services/IReservaService.cs
@@ -2,8 +2,9 @@
 /// Defines the contract for reservation-related operations within the application.
 /// Implement this interface to provide methods for managing reservations.
 /// </summary>
+namespace areas_deportivas.Services;
 public interface IReservaService
 {
-	Task Reservar();
+	Task Reservar(CrearReservaDto crearReserva);
 	Task CancelarReserva();
 }
