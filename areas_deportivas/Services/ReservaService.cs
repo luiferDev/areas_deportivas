b89f5b81bf79a9d904ba9675c3d2c2ed755d96ae
luifer991 <luifer991@gmail.com>
2025-06-12 22:09:16 -0500

    Refactor ReservaService to implement Reservar method with CrearReservaDto parameter for reservation handling

--- areas_deportivas/Services/ReservaService.cs
+++ areas_deportivas/Services/ReservaService.cs
@@ -1,13 +1,44 @@
 
+using areas_deportivas.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace areas_deportivas.Services;
+
 public class ReservaService : IReservaService
 {
+	private readonly DeportesDbContext _context;
+
+	public ReservaService(DeportesDbContext context)
+	{
+		_context = context;
+	}
 	public Task CancelarReserva()
 	{
 		throw new NotImplementedException();
 	}
 
-	public Task Reservar()
+	public async Task Reservar(CrearReservaDto crearReserva)
 	{
-		throw new NotImplementedException();
+		try
+		{
+			// Assuming crearReserva contains the user ID, otherwise you need to pass it as a parameter
+			var userId = crearReserva.IdUsuario;
+			var reserva = new Reserva
+			{
+				Fecha = crearReserva.Fecha,
+				HoraInicio = crearReserva.HoraInicio,
+				HoraFin = crearReserva.HoraFin,
+				IdUsuario = userId,
+				IdAreaDeportiva = crearReserva.IdAreaDeportiva
+			};
+
+			await _context.Reservas.AddAsync(reserva);
+			await _context.SaveChangesAsync();
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex.Message);
+		}
 	}
 }
