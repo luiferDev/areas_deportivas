e06ee375ce6ad188344c3005c64f31454e2b2a5f
luifer991 <luifer991@gmail.com>
2025-06-12 22:11:18 -0500

    Add AreaDeportivaService class with CambiarDisponibilidad method stub

---
+++ areas_deportivas/Services/AreaDeportivaService.cs
@@ -0,0 +1,9 @@
+
+namespace areas_deportivas.Services;
+public class AreaDeportivaService : IAreaDeportivaService
+{
+	public Task CambiarDisponibilidad()
+	{
+		throw new NotImplementedException();
+	}
+}
