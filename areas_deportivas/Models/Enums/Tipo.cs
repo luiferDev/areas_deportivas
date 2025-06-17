6401d3b6e99d2f0dbd15bb4892202ff518051a04
luifer991 <luifer991@gmail.com>
2025-06-17 16:53:49 -0500

    refactor matching types whit the enum db

--- areas_deportivas/Models/Enums/Tipo.cs
+++ areas_deportivas/Models/Enums/Tipo.cs
@@ -1,9 +1,17 @@
+using NpgsqlTypes;
+
 public enum Tipo
 {
+	[PgName("FOOTBALL")]
 	FOOTBALL,
+	[PgName("TENIS")]
 	TENIS,
+	[PgName("BASKETBALL")]
 	BASKETBALL,
+	[PgName("GIMNASIO")]
 	GIMNASIO,
+	[PgName("BILLAR")]
 	BILLAR,
+	[PgName("PISCINA")]
 	PISCINA
 }
