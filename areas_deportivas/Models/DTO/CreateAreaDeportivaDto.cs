8a0952b97d18b3a04f879fbf657066eb93ee339e
luifer991 <luifer991@gmail.com>
2025-06-19 13:13:38 -0500

    add include the other properties for the CreateAreaDeportivaDto.cs

--- areas_deportivas/Models/DTO/CreateAreaDeportivaDto.cs
+++ areas_deportivas/Models/DTO/CreateAreaDeportivaDto.cs
@@ -1,8 +1,16 @@
+using areas_deportivas.Models.Enums;
+
 namespace areas_deportivas.Models.DTO;
 
 public class CreateAreaDeportivaDto
 {
 	public string Nombre { get; set; } = null!;
+	
+	public string Descripcion { get; set; } = null!;
 
+	public string ImageUrl { get; set; } = null!;
+	
 	public Tipo TipoArea { get; set; }
+	
+	public decimal Precio { get; set; }
 }
