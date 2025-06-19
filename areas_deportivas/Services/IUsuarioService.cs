75fb78940d682a02624e39f0e9455157d8ec2095
luifer991 <luifer991@gmail.com>
2025-06-19 13:15:47 -0500

    add method for updating and deleting reservations

--- areas_deportivas/Services/IUsuarioService.cs
+++ areas_deportivas/Services/IUsuarioService.cs
@@ -6,4 +6,6 @@ public interface IUsuarioService
 {
 	Task<ReservaRespuestaDto> ReservarAreaAsync(CrearReservaDto crearReserva, int Id, Guid userId);
 	Task CancelarAreaAsync(Guid userId);
+	Task EliminarAreaAsync(Guid reservaId);
+	Task ActualizarAreaAsync(Guid reservaId, ActualizarReservaDto actualizarReserva);
 }
